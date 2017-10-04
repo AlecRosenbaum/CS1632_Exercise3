0ed9242e2eabc30f29e06ae0001f62f20c073693
Alec Rosenbaum <alecr95@gmail.com>
2017-10-04 10:15:05 -0400

    semicolon

--- src/RentACat.java
+++ src/RentACat.java
@@ -224,7 +224,7 @@ public class RentACat {
                             }
                         } catch (Exception nfex) {
                             System.err.println("Invalid cat ID.");
-                            sc.next()
+                            sc.next();
                         }
                     }
 
